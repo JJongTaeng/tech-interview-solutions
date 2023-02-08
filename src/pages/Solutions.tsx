d38cf7c1a00df167385ac4135e1d12622fca19c5
intae <jjojjo141995@gmail.com>
2023-02-08 17:21:04 +0900

    feat: mock 데이터 추가, 문제 페이지 작업

--- src/pages/Solutions.tsx
+++ src/pages/Solutions.tsx
@@ -1,15 +1,59 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
-import { subjectAtom } from '../store/setting';
+import React, { useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Button, Card, message, Radio, RadioChangeEvent, Space } from 'antd';
+import { nanoid } from 'nanoid';
+import solutionList from '../mock/mockSolutions';
+import { ROUTEPATH } from '../types';
 
 function Solutions() {
-  const { number } = useParams();
-  const subject = useRecoilValue(subjectAtom);
+  const [value, setValue] = useState();
+  const param = useParams<{ number: string }>();
+  const navigate = useNavigate();
+  if (!param.number) throw new Error('solution number invalid');
+  const pageNumber = parseInt(param.number, 10);
+  const isLastSolution = solutionList.length === pageNumber;
+  const isExceedSolutionNumber = solutionList.length < pageNumber;
+  const solution = solutionList[pageNumber - 1];
+  const nextPageNumber = pageNumber + 1;
+
+  if (isExceedSolutionNumber) {
+    navigate(`${ROUTEPATH.SOLUTIONS}/${solutionList.length}`);
+  }
+
+  const onChange = (e: RadioChangeEvent) => {
+    setValue(e.target.value);
+  };
+
   return (
-    <div>
-      solutions subject = {subject} number = {number}
-    </div>
+    <Card
+      title={`${pageNumber}. ${solution.title}`}
+      actions={[
+        <Button
+          key={nanoid()}
+          onClick={() => {
+            if (isLastSolution) {
+              message.info('마지막 문제입니다.');
+              return;
+            }
+            navigate(`${ROUTEPATH.SOLUTIONS}/${nextPageNumber}`);
+          }}
+        >
+          다음문제
+        </Button>,
+      ]}
+    >
+      <Radio.Group onChange={onChange} value={value}>
+        <Space direction="vertical">
+          {solution.list.map((item, index) => (
+            <div key={nanoid()}>
+              <Radio value={index}>
+                {index + 1}. {item}
+              </Radio>
+            </div>
+          ))}
+        </Space>
+      </Radio.Group>
+    </Card>
   );
 }
 
